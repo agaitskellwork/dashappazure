b7aec77c33903b583ac7fc9260afe2d479505fef
agaitskellwork <100278937+agaitskellwork@users.noreply.github.com>
2022-03-01 14:53:50 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+import notebook_importing
+
+import nbpackage.dashapp as ds
+
+app = Dash(__name__)
+app.layout = ds.layout
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
 
